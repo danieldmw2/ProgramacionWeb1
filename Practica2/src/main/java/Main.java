2899e886e31b080aceb7049797cb5922c45c545b
danieldmw2 <daniel.dmw@gmail.com>
2016-05-25 02:03:18 -0400

    Practica 2: UI Kit Agregado

--- Practica2/src/main/java/Main.java
+++ Practica2/src/main/java/Main.java
@@ -1,10 +1,29 @@
 /**
  * Created by Daniel's Laptop on 5/25/2016.
  */
+
+import freemarker.template.Configuration;
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        staticFiles.location("/public");
 
+        Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(Main.class, "/html");
+        FreeMarkerEngine freeMarker = new FreeMarkerEngine();
+        freeMarker.setConfiguration(configuration);
+
+        get("/test", (req, res) -> {
+            return new ModelAndView(null, "randomTest.html");
+        }, freeMarker);
     }
 }
