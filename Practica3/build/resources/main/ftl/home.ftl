d3b3cf6d5c0bd90c0c3f89ad45f03ed803097140
danieldmw2 <daniel.dmw@gmail.com>
2016-05-29 17:36:35 -0400

    Practica 3: Bootstrap 3 Agregado

---
+++ Practica3/build/resources/main/ftl/home.ftl
@@ -0,0 +1,3 @@
+<html>
+    Super Sexo
+</html>
--- Practica3/src/main/java/Main.java
+++ Practica3/src/main/java/Main.java
@@ -1,10 +1,26 @@
 /**
  * Created by Daniel's Laptop on 5/29/2016.
  */
+
+import spark.ModelAndView;
+import freemarker.template.Configuration;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import static spark.Spark.*;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        staticFiles.location("/public");
 
+        Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(Main.class, "/ftl");
+        FreeMarkerEngine freeMarker = new FreeMarkerEngine();
+        freeMarker.setConfiguration(configuration);
+
+        get("/home", (req, res) -> {
+            return new ModelAndView(null, "home.ftl");
+        }, freeMarker);
     }
 }
