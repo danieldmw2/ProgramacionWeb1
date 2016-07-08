dacd821beac15af028f84ca2c8cf54b3a690cad9
Ariel Salce <arielsalcepk@gmail.com>
2016-07-07 21:05:58 -0400

    Practica6: Estudiantes Services Completado

--- Practica6/src/webservices/EstudianteServices.java
+++ Practica6/src/webservices/EstudianteServices.java
@@ -6,6 +6,8 @@ import javax.jws.WebMethod;
 import javax.jws.WebService;
 import java.util.ArrayList;
 
+import static main.Server.estudiantes;
+
 /**
  * Created by Daniel's Laptop on 7/7/2016.
  */
@@ -15,30 +17,45 @@ public class EstudianteServices
     @WebMethod
     public void insert(int matricula, String nombre, String carrera)
     {
-
+        estudiantes.add(new Estudiante(matricula, nombre, carrera));
     }
 
     @WebMethod
     public void update(int matricula, String nombre, String carrera)
     {
-
+        for(int i = 0; i < estudiantes.size(); i++)
+        {
+            if(estudiantes.get(i).getMatricula() == matricula)
+            {
+                estudiantes.get(i).setCarrera(carrera);
+                estudiantes.get(i).setNombre(nombre);
+            }
+        }
     }
 
     @WebMethod
     public void delete(int matricula)
     {
-
+        for(int i = 0; i < estudiantes.size(); i++)
+        {
+            if (estudiantes.get(i).getMatricula() == matricula)
+            {
+                estudiantes.remove(i);
+                return;
+            }
+        }
     }
 
     @WebMethod
-    public ArrayList<Estudiante> select()
-    {
-        return new ArrayList<>();
-    }
+    public ArrayList<Estudiante> select() { return estudiantes; }
 
     @WebMethod
     public Estudiante selectByID(int matricula)
     {
-        return new Estudiante(1, "2", "3");
+        for(int i = 0; i < estudiantes.size(); i++)
+            if(estudiantes.get(i).getMatricula() == matricula)
+                return estudiantes.get(i);
+
+        return null;
     }
 }
