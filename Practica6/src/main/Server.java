9b653a34d0c2a71cf63dfa5581f90d7722c861e6
danieldmw2 <daniel.dmw@gmail.com>
2016-07-07 20:40:16 -0400

    Practica 6: Lista de Estudiantes Agregada

--- Practica6/src/main/Server.java
+++ Practica6/src/main/Server.java
@@ -1,16 +1,19 @@
 package main;
 
-import example.HelloWorld;
+import domain.Estudiante;
 import webservices.AsignaturaServices;
 import webservices.EstudianteServices;
 
 import javax.xml.ws.Endpoint;
+import java.util.ArrayList;
 
 /**
  * Created by Daniel's Laptop on 7/7/2016.
  */
 public class Server
 {
+    public static ArrayList<Estudiante> estudiantes;
+
     public static void main(String[] argv) throws Exception
     {
         Endpoint.publish("http://localhost:9000/Estudiantes", new EstudianteServices());
