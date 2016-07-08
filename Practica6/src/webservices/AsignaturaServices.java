f15e24872531d0fe36da06338925697ee303d5e9
danieldmw2 <daniel.dmw@gmail.com>
2016-07-07 21:04:07 -0400

    Practica 6: Asignatura Service Completado

--- Practica6/src/webservices/AsignaturaServices.java
+++ Practica6/src/webservices/AsignaturaServices.java
@@ -1,10 +1,14 @@
 package webservices;
 
 import domain.Asignatura;
+import domain.Estudiante;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import java.util.ArrayList;
+import java.util.List;
+
+import static main.Server.estudiantes;
 
 /**
  * Created by Daniel's Laptop on 7/7/2016.
@@ -15,30 +19,59 @@ public class AsignaturaServices
     @WebMethod
     public void insert(int matricula, String codigo, String nombre)
     {
-
+        for (int i = 0; i < estudiantes.size(); i++)
+            if (estudiantes.get(i).getMatricula() == matricula)
+                estudiantes.get(i).addAsignatura(new Asignatura(codigo, nombre));
     }
 
     @WebMethod
     public void update(int matricula, String codigo, String nombre)
     {
-
+        for (int i = 0; i < estudiantes.size(); i++)
+            if (estudiantes.get(i).getMatricula() == matricula)
+                for (int j = 0; j < estudiantes.get(i).getAsignaturas().size(); j++)
+                    if (estudiantes.get(i).getAsignaturas().get(j).getCodigo().equals(codigo))
+                        estudiantes.get(i).getAsignaturas().get(j).setNombre(nombre);
     }
 
     @WebMethod
     public void remove(int matricula, String codigo)
     {
-
+        for (int i = 0; i < estudiantes.size(); i++)
+        {
+            if (estudiantes.get(i).getMatricula() == matricula)
+            {
+                for (int j = 0; j < estudiantes.get(i).getAsignaturas().size(); j++)
+                {
+                    if (estudiantes.get(i).getAsignaturas().get(j).getCodigo().equals(codigo))
+                    {
+                        estudiantes.get(i).getAsignaturas().remove(j);
+                        j--;
+                    }
+                }
+            }
+        }
     }
 
     @WebMethod
-    public ArrayList<Asignatura> select()
+    public List<Asignatura> select(int matricula)
     {
+        for(Estudiante e : estudiantes)
+            if(e.getMatricula() == matricula)
+                return e.getAsignaturas();
+
         return new ArrayList<>();
     }
 
     @WebMethod
     public Asignatura selectByIDs(int matricula, String codigo)
     {
-        return new Asignatura("1", "2");
+        for(Estudiante e : estudiantes)
+            if(e.getMatricula() == matricula)
+                for (Asignatura a : e.getAsignaturas())
+                    if(a.getCodigo().equals(codigo))
+                        return a;
+
+        return null;
     }
 }
